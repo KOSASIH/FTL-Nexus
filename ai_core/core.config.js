73846019cacfb56936e7e3a53c08c654bc47f2b0
KOSASIH <kosasihg88@gmail.com>
2024-07-15 10:54:38 +0700

    Create core.config.js

---
+++ ai_core/core.config.js
@@ -0,0 +1,18 @@
+export default {
+  neural_network: {
+    architecture: 'transformer',
+    hidden_layers: 6
+  },
+  natural_language_processing: {
+    language: 'english',
+    model: 'bert'
+  },
+  computer_vision: {
+    model: 'resnet50',
+    resolution: 1024
+  },
+  robotics: {
+    platform: 'ros',
+    sensors: ['lidar', 'camera']
+  }
+};
