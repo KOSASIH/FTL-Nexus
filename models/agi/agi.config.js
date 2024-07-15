70acd9c2ecc0a522051ec5f2ae08ed192c2793c9
KOSASIH <kosasihg88@gmail.com>
2024-07-15 10:44:33 +0700

    Create agi.config.js

---
+++ models/agi/agi.config.js
@@ -0,0 +1,15 @@
+export default {
+  brain: {
+    hiddenLayers: [128, 256, 128],
+    learningRate: 0.01
+  },
+  tensorflow: {
+    model: 'mobilenet_v2'
+  },
+  natural: {
+    language: 'en'
+  },
+  memory: {
+    capacity: 1000
+  }
+};
