be79ea7941e07f0a292916aad6713031217667b4
KOSASIH <kosasihg88@gmail.com>
2024-07-15 10:51:04 +0700

    Create sensors.js

---
+++ advanced_ai_pilot/pilot.modules/sensors.js
@@ -0,0 +1,19 @@
+class Sensors {
+  constructor() {
+    this.type = 'lidar';
+    this.range = 100;
+  }
+
+  async init() {
+    // Initialize the sensor system
+  }
+
+  async read() {
+    // Read the sensor data
+    const sensorData = [];
+    //...
+    return sensorData;
+  }
+}
+
+export default Sensors;
