cf9e1351a0697a0bddd56212b1d2090cffa87307
KOSASIH <kosasihg88@gmail.com>
2024-07-15 11:15:03 +0700

    Create core.js

---
+++ data_analysis/analysis.modules/core.js
@@ -0,0 +1,18 @@
+class Core {
+  constructor() {
+    // Initialize the core system
+  }
+
+  async getInput() {
+    // Get the input for the data analysis system
+    const input = [];
+    //...
+    return input;
+  }
+
+  async setOutput(output) {
+    // Set the output for the data analysis system
+  }
+}
+
+export default Core;
