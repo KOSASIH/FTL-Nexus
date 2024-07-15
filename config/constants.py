065f1982e6f20b9159ebad71c2a840141461929a
KOSASIH <kosasihg88@gmail.com>
2024-07-15 09:41:33 +0700

    Create constants.py

---
+++ config/constants.py
@@ -0,0 +1,11 @@
+# Quantum simulation constants
+NUM_QUBITS = 10
+ENTANGLEMENT_THRESHOLD = 0.5
+
+# AI navigation constants
+MODEL_TYPE = "nav_model"
+TRAINING_DATA = "nav_data.csv"
+
+# Propulsion system constants
+ENERGY_SOURCE = "quantum_fluctuations"
+ACCELERATION_RATE = 10.0
