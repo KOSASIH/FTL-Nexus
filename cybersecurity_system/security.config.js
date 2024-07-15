fba6af576cc574f3896c83c92d8f75be61fdcdaf
KOSASIH <kosasihg88@gmail.com>
2024-07-15 11:03:23 +0700

    Create security.config.js

---
+++ cybersecurity_system/security.config.js
@@ -0,0 +1,18 @@
+export default {
+  intrusion_detection: {
+    algorithm: 'anomaly_detection',
+    sensitivity: 0.9
+  },
+  vulnerability_assessment: {
+    frequency: 'daily',
+    scope: 'network'
+  },
+  incident_response: {
+    team: 'cybersecurity_team',
+    protocol: 'nist_800_61'
+  },
+  access_control: {
+    authentication: 'multi_factor',
+    authorization: 'role_based'
+  }
+};
