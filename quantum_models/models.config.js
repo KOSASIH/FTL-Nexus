133da981fdd6e2055c7b9b3c5685cfc5e1bcd630
KOSASIH <kosasihg88@gmail.com>
2024-07-15 11:36:01 +0700

    Create models.config.js

---
+++ quantum_models/models.config.js
@@ -0,0 +1,18 @@
+export default {
+  quantum_neural_network: {
+    architecture: 'feedforward',
+    num_layers: 5
+  },
+  quantum_support_vector_machine: {
+    kernel: 'gaussian',
+    regularization: 0.1
+  },
+  quantum_kmeans: {
+    num_clusters: 10,
+    distance_metric: 'euclidean'
+  },
+  quantum_regression: {
+    algorithm: 'least_squares',
+    num_iterations: 1000
+  }
+};
