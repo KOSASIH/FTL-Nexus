62d1bd38ff2f3ff071d143426d6f6b9e45743722
KOSASIH <kosasihg88@gmail.com>
2024-07-15 11:16:03 +0700

    Create drive.config.js

---
+++ ftl_drive/drive.config.js
@@ -0,0 +1,22 @@
+export default {
+  ftl_core: {
+    algorithm: 'exotic_matter',
+    power_output: 1000
+  },
+  energy_source: {
+    type: 'antimatter',
+    capacity: 10000
+  },
+  propulsion_system: {
+    type: 'warp_drive',
+    speed: 10
+  },
+  navigation_system: {
+    type: 'inertial_measurement_unit',
+    accuracy: 0.01
+  },
+  stabilization_system: {
+    type: 'gyroscopic_stabilizer',
+    precision: 0.001
+  }
+};
