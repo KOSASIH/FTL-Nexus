97c1d5c21eb60f2e249c4308a74ef936df0d654a
KOSASIH <kosasihg88@gmail.com>
2024-07-15 11:40:08 +0700

    Create core.js

---
+++ quantum_models/models.modules/core.js
@@ -0,0 +1,18 @@
+class Core {
+  constructor() {
+    // Initialize the core system
+  }
+
+  async getInput() {
+    // Get the input for the quantum models system
+    const input = [];
+    //...
+    return input;
+  }
+
+  asyncsetOutput(output) {
+    // Set the output for the quantum models system
+  }
+}
+
+export default Core;
